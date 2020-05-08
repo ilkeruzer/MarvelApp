4ca65ffe9db7bff8b49b55d5ea7b40c1e3cb78be
İlker Üzer <ilkeruzer35@gmail.com>
2020-05-08 18:40:28 +0300

    BaseFragment ve View Binding yapısı oluşturuldu.

--- app/src/main/java/com/ilkeruzer/marvel/view/home/HomeFragment.kt
+++ app/src/main/java/com/ilkeruzer/marvel/view/home/HomeFragment.kt
@@ -1,34 +1,37 @@
 package com.ilkeruzer.marvel.view.home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.ilkeruzer.marvel.R
+import com.ilkeruzer.marvel.base.BaseFragment
+import com.ilkeruzer.marvel.databinding.FragmentHomeBinding
 import org.koin.android.ext.android.inject
 
 
-class HomeFragment : Fragment() {
+class HomeFragment : BaseFragment<HomeViewModel>() {
 
-    private val viewModel by inject<HomeViewModel>()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
+    private val vM by inject<HomeViewModel>()
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel.postsAPI()
+        binding.textView.text = "Test Test"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        viewModel = vM
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun getViewBindingRoot(inflater: LayoutInflater,container: ViewGroup?): View? {
+        binding = FragmentHomeBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun getViewModel(): Class<HomeViewModel> {
+       return HomeViewModel::class.java
     }
 
 
